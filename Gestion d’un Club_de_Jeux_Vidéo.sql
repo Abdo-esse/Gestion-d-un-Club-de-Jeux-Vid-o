fe6e173be2bf14ef16593591e30dd3c8621a0ca0
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-09 21:23:00 +0100

    j'ai creat tableau tableau

--- Gestion d’un Club_de_Jeux_Vidéo.sql
+++ Gestion d’un Club_de_Jeux_Vidéo.sql
@@ -1,3 +1,10 @@
 -- Active: 1733434204073@@127.0.0.1@3306
 create database Club_de_Jeux_Vidéo;
 use Club_de_Jeux_Vidéo;
+
+CREATE table Abonnement(
+    id int not null PRIMARY key AUTO_INCREMENT,
+    type_Abonnement VARCHAR(50) not null,
+    data_debut  date,
+    date_fin  date
+);
