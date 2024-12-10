4e30c3bd6944af0cb99816732f9ddde03d594546
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-10 23:22:53 +0100

    j'ai termine la requet  --Afficher la liste des membres et le type d'abonnement auquel ils sont associés.

--- query.sql
+++ query.sql
@@ -26,3 +26,20 @@ select * from MEMBRE where id_Abonnement=1;
        Emprunter
  inner join MEMBRE on MEMBRE.id=Emprunter.id_Member
  inner join jeu on jeu.id=Emprunter.id_Member
+
+-- Lister tous les emprunts, en incluant le pseudo du membre et les informations sur le jeu (titre et studio de développement).
+
+select
+        jeu.titre,MEMBRE.full_name,jeu.studio_develepement
+ FROM
+       Emprunter
+ inner join MEMBRE on MEMBRE.id=Emprunter.id_Member
+ inner join jeu on jeu.id=Emprunter.id_Member
+
+ --Afficher la liste des membres et le type d'abonnement auquel ils sont associés.
+
+ SELECT 
+        MEMBRE.full_name,MEMBRE.date_rejoint,Abonnement. type_Abonnement
+ FROM
+       MEMBRE
+inner join Abonnement on Abonnement.id=MEMBRE.id_Abonnement
