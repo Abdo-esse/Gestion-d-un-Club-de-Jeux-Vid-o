b895f554d132de952daef81659ee09df0a7da7df
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-10 15:21:35 +0100

    j'ai termine Lister les membres ayant participé à un tournoi, avec leur pseudo, le nom du tournoi, et leur rang final.

--- query.sql
+++ query.sql
@@ -1,2 +1,15 @@
 -- Active: 1733434204073@@127.0.0.1@3306
 use Club_de_Jeux_Vidéo;
+show tables;
+select * FROM participer;
+
+
+
+select
+    MEMBRE.full_name, 
+    Tournoi.nom_tournoi, 
+    Participer.range_final
+from 
+    Participer
+inner join MEMBRE on MEMBRE.id = Participer.id_Member
+inner join Tournoi on Tournoi.id = Participer.id_Tournoi;
